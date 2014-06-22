8d65db68daa7d4068e20f5d1e34d4d3cd5822cce
labes <0x1abe5@gmail.com>
2014-06-23 01:07:08 +0200

    reporting service generation errors

--- plugin/src/main/java/dev/deadc0de/genesis/maven/plugin/RunGoal.java
+++ plugin/src/main/java/dev/deadc0de/genesis/maven/plugin/RunGoal.java
@@ -2,6 +2,7 @@ package dev.deadc0de.genesis.maven.plugin;
 
 import dev.deadc0de.genesis.GenerationContext;
 import dev.deadc0de.genesis.ServiceFactory;
+import dev.deadc0de.genesis.ServiceGenerationException;
 import dev.deadc0de.genesis.module.ServiceModule;
 import java.util.ServiceLoader;
 import java.util.stream.Stream;
@@ -25,13 +26,25 @@ public class RunGoal extends AbstractMojo {
         }
         final Stream<ServiceFactory> serviceFactories = loadServiceFactories();
         final GenerationContext context = new GenerationContext(serviceFactories);
-        final Runnable rootService = context.generate(Runnable.class, service.toServiceDescriptor());
-        rootService.run();
+        try {
+            final Runnable rootService = context.generate(Runnable.class, service.toServiceDescriptor());
+            rootService.run();
+        } catch (ServiceGenerationException exception) {
+            throw new MojoExecutionException(formatErrorMessage(exception), exception);
+        }
     }
 
     private static Stream<ServiceFactory> loadServiceFactories() {
         final ServiceLoader<ServiceModule> loader = ServiceLoader.load(ServiceModule.class);
         final Stream<ServiceModule> serviceModules = StreamSupport.stream(loader.spliterator(), false);
         return serviceModules.flatMap(ServiceModule::assemble);
+    }
+
+    private static String formatErrorMessage(ServiceGenerationException exception) {
+        final StringBuilder builder = new StringBuilder(exception.getMessage());
+        for (ServiceFactory<?> serviceFactory : exception.serviceGenerationStack()) {
+            builder.append(String.format("%nat service %s", serviceFactory));
+        }
+        return builder.toString();
     }
 }
